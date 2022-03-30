7b1e6a64b6f4a0c0da89b621f9f509a95afba58a
George Hett <georgehett95@gmail.com>
2022-03-30 16:42:41 +0100

    weather api functionality added to lookup by city

--- weather.js
+++ weather.js
@@ -4,19 +4,17 @@ import got from "got";
 const apiKey = process.env.WEATHER_KEY;
 
 class WeatherApi {
-  constructor() {
-    const city = "London";
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
-    let weatherData = null;
+  fetchWeatherData(city, callback) {
+    const place = city;
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${place}&appid=${apiKey}`;
+    got(apiUrl).then((response) => {
+      const weatherData = JSON.parse(response.body);
+      callback(weatherData);
+    });
   }
 }
 
-let weatherData = null;
-
-got(apiUrl).then((response) => {
-  weatherData = JSON.parse(response.body);
-  console.log(weatherData);
-  console.log(weatherData.main.temp);
-  console.log(weatherData.visibility);
-  console.log(weatherData.weather[0].main);
+const weather = new WeatherApi();
+weather.fetchWeatherData("New Delhi", (weatherData) => {
+  console.log(weatherData.main);
 });
